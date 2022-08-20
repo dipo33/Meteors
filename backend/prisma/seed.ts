07b6265dec1a2e5ce587fa6cb4839828384f3b3e
Matej Dipčár <mato.dipcar@gmail.com>
2022-08-20 19:42:44 +0200

    Create simple test seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,6 +4,54 @@ const prisma = new PrismaClient();
 
 async function main() {
   console.log('Running seed.ts');
+
+  const testMeteor = await prisma.meteor.create({
+    data: {
+      name: 'Test Meteor',
+      cost: 69420,
+      radius: 42,
+    },
+  });
+
+  const oreIron = await prisma.meteorItem.create({
+    data: {
+      name: 'Iron Ore',
+      weight: 50,
+      meteorId: testMeteor.id,
+    },
+  });
+
+  const oreCopper = await prisma.meteorItem.create({
+    data: {
+      name: 'Copper Ore',
+      weight: 50,
+      meteorId: testMeteor.id,
+    },
+  });
+
+  const oreTin = await prisma.meteorItem.create({
+    data: {
+      name: 'Tin Ore',
+      weight: 50,
+      meteorId: testMeteor.id,
+    },
+  });
+
+  const oreSilver = await prisma.meteorItem.create({
+    data: {
+      name: 'Silver Ore',
+      weight: 50,
+      meteorId: testMeteor.id,
+    },
+  });
+
+  const oreLead = await prisma.meteorItem.create({
+    data: {
+      name: 'Lead Ore',
+      weight: 50,
+      meteorId: testMeteor.id,
+    },
+  });
 }
 
 main()
