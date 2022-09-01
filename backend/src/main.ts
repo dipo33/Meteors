a86f18fa721e70ec8924410ab03a12ad259edfba
Matej Dipčár <mato.dipcar@gmail.com>
2022-09-01 19:21:23 +0200

    Add support for https

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import * as fs from 'fs';
+import 'dotenv/config';
 
-const port = 3000;
+const port = process.env.BACKEND_PORT;
+const host = process.env.BACKEND_HOST;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(port);
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERTIFICATE),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(port, host);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
